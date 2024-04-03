ede35bcc1d591974feeb1e76c8d6fc2f36936174
Xichen Pan <xichen.pan@gmail.com>
2024-04-03 01:54:28 -0700

    init shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,14 @@
-import { useAccount, useConnect, useDisconnect } from 'wagmi'
+import { useAccount, useConnect, useDisconnect } from 'wagmi';
 
 function App() {
-  const account = useAccount()
-  const { connectors, connect, status, error } = useConnect()
-  const { disconnect } = useDisconnect()
+  const account = useAccount();
+  const { connectors, connect, status, error } = useConnect();
+  const { disconnect } = useDisconnect();
 
   return (
     <>
       <div>
         <h2>Account</h2>
-
         <div>
           status: {account.status}
           <br />
@@ -40,7 +39,7 @@ function App() {
         <div>{error?.message}</div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/wagmi.ts
+++ frontend/src/wagmi.ts
@@ -1,6 +1,6 @@
-import { http, createConfig } from 'wagmi'
-import { mainnet, sepolia } from 'wagmi/chains'
-import { coinbaseWallet, injected, walletConnect } from 'wagmi/connectors'
+import { http, createConfig } from 'wagmi';
+import { mainnet, sepolia } from 'wagmi/chains';
+import { coinbaseWallet, injected, walletConnect } from 'wagmi/connectors';
 
 export const config = createConfig({
   chains: [mainnet, sepolia],
@@ -13,10 +13,10 @@ export const config = createConfig({
     [mainnet.id]: http(),
     [sepolia.id]: http(),
   },
-})
+});
 
 declare module 'wagmi' {
   interface Register {
-    config: typeof config
+    config: typeof config;
   }
 }
